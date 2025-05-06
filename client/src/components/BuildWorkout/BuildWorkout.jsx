96545e08f94165d148379ea95eea13a4af4d7700
ashdrey1110 <ashrey1110@gmail.com>
2025-05-06 15:09:22 -0700

    started frontend

---
+++ client/src/components/BuildWorkout/BuildWorkout.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import "./BuildWorkout.css";
+import { useNavigate, Link } from "react-router-dom";
+
+function BuildWorkout() {
+  //your code here
+
+  return (
+    <>
+      <h1> Hello this is the BuildWorkout page!</h1>
+    </>
+  );
+}
+
+export default BuildWorkout;
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import "./Home.css";
+import { useNavigate, Link } from "react-router-dom";
+
+function Home() {
+  //your code here
+  return (
+    <>
+      <h1> Hello this is the home page!</h1>
+    </>
+  );
+}
+
+export default Home;
