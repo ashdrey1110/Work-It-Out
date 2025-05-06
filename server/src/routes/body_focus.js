dc20447a1d75568e1704c89b726399ff4c296734
ashdrey1110 <ashrey1110@gmail.com>
2025-05-06 10:12:26 -0700

    added more exercise routes

--- server/src/routes/body_focus.js
+++ server/src/routes/body_focus.js
@@ -5,18 +5,18 @@ const knex = require("knex")(require("../../knexfile")["development"]);
 router.get("/", (req, res) => {
   knex("body_focus")
     .select("*")
-    .then((users) => res.status(200).json(users))
+    .then((body_focus) => res.status(200).json(body_focus))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
-// TEMPORARY, not spending much time with users since it's only meant for one person right now
+// TEMPORARY, not spending much time with body_focus since it's only meant for one person right now
 
 router.get("/:id", (req, res) => {
   let id = req.params.id;
   knex("body_focus")
     .select()
     .where("id", id)
-    .then((users) => res.status(200).json(users))
+    .then((body_focus) => res.status(200).json(body_focus))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -25,7 +25,7 @@ router.get("/name/:name", (req, res) => {
   knex("body_focus")
     .select()
     .whereILike("name", `%${name}%`)
-    .then((users) => res.status(200).json(users))
+    .then((body_focus) => res.status(200).json(body_focus))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
--- server/src/routes/exercises.js
+++ server/src/routes/exercises.js
@@ -5,7 +5,7 @@ const knex = require("knex")(require("../../knexfile")["development"]);
 router.get("/", (req, res) => {
   knex("exercises")
     .select("*")
-    .then((users) => res.status(200).json(users))
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -14,7 +14,7 @@ router.get("/id/:id", (req, res) => {
   knex("exercises")
     .select()
     .where("id", id)
-    .then((users) => res.status(200).json(users))
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -23,18 +23,58 @@ router.get("/name/:name", (req, res) => {
   knex("exercises")
     .select()
     .whereILike("name", `${name}`)
-    .then((users) => res.status(200).json(users))
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+// SPECIFIC LIST BUILDER
+router.get("/specific/:effort/:focus/:type/:sport", (req, res) => {
+  let effort = parseInt(req.params.effort);
+  let focus = req.params.focus;
+  let type = req.params.type;
+  let sport = req.params.sport;
+  knex("exercises")
+    .select()
+    .whereBetween("effort", [effort - 1, effort + 1])
+    .where(`${focus}`, ">", 2)
+    .where(`${type}`, ">", 2)
+    .where("sport", sport)
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
 // EFFORT: unsure how to best use this route
 
 router.get("/effort/:effort", (req, res) => {
-  let effort = req.params.effort;
+  let effort = parseInt(req.params.effort);
   knex("exercises")
     .select()
     .where("effort", effort)
-    .then((users) => res.status(200).json(users))
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/high-effort", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("effort", ">", 3)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/low-effort", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("effort", "<", 4)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/medium-effort", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("effort", ">", 2)
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -44,7 +84,7 @@ router.get("/warmup", (req, res) => {
     .select()
     .where("aerobic_cardio", ">", 3)
     .where("sport", false)
-    .then((users) => res.status(200).json(users))
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -54,7 +94,7 @@ router.get("/strengthcardio", (req, res) => {
     .select()
     .where("aerobic_cardio", ">", 3)
     .where("sport", false)
-    .then((users) => res.status(200).json(users))
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -62,7 +102,7 @@ router.get("/sports", (req, res) => {
   knex("exercises")
     .select()
     .where("sport", true)
-    .then((users) => res.status(200).json(users))
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -70,7 +110,8 @@ router.get("/upper_body", (req, res) => {
   knex("exercises")
     .select()
     .where("upper_body", ">", 2)
-    .then((users) => res.status(200).json(users))
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
@@ -78,7 +119,104 @@ router.get("/lower_body", (req, res) => {
   knex("exercises")
     .select()
     .where("lower_body", ">", 2)
-    .then((users) => res.status(200).json(users))
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/core", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("core", ">", 2)
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/full_body", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("full_body", ">", 2)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/arms", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("arms", ">", 2)
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/legs", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("legs", ">", 2)
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/glutes", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("glutes", ">", 3)
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/aerobic", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("aerobic_cardio", ">", 2)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/anaerobic", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("anaerobic_cardio", ">", 2)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/strength", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("strength", ">", 2)
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/flexibility", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("flexibility", ">", 2)
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/balance", (req, res) => {
+  knex("exercises")
+    .select()
+    .where("balance", ">", 2)
+    .where("sport", false)
+    .then((exercises) => res.status(200).json(exercises))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+router.get("/search/:searchterm", (req, res) => {
+  let searchterm = req.params.searchterm;
+  knex("exercises")
+    .select()
+    .whereILike("name", `%${searchterm}%`)
+    .then((exercises) => res.status(200).json(exercises))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
 
